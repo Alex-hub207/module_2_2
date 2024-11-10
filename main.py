ab2f92adbda90f57ae0c27c9ceb5e4d9a71a9220
Alex-hub207 <n77344167@gmail.com>
2024-11-10 20:22:08 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+first = int(input())
+second = int(input())
+third = int(input())
+if first == second and second == third:
+    print()
+    print(3)
+elif first == second or first == third or second == third:
+    print()
+    print(2)
+else:
+    print()
+    print(0)
